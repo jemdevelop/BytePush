0b1856cee38fd7c59a05afd46fb9a2a09969b754
jonathanmonreal <jem177@case.edu>
2018-08-28 22:52:48 -0600

    add properties to BytePushTag

--- BytePush/BytePushTag.swift
+++ BytePush/BytePushTag.swift
@@ -9,5 +9,22 @@
 import Foundation
 
 public struct BytePushTag: Codable {
+    enum Taxonomy: String, Codable {
+        case category, post_tag, nav_menu, link_category, post_format
+    }
     
+    /// Unique identifier for the term.
+    let id: Int
+    /// Number of published posts for the term.
+    let count: Int
+    /// HTML description of the term.
+    let description: String
+    /// URL of the term.
+    let link: URL
+    /// HTML title for the term.
+    let name: String
+    /// An alphanumeric identifier for the term unique to its type.
+    let slug: String
+    /// Type attribution for the term.
+    let taxonomy: Taxonomy
 }
