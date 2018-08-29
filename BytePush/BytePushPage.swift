3fa116b3ea9505aa90c1ad8d46fead02c4243eb8
jonathanmonreal <jem177@case.edu>
2018-08-28 22:57:18 -0600

    add BytePushPage properties

--- BytePush/BytePushPage.swift
+++ BytePush/BytePushPage.swift
@@ -9,5 +9,79 @@
 import Foundation
 
 public struct BytePushPage: Codable {
+    enum Status: String, Codable {
+        case publish = "publish"
+        case future = "future"
+        case draft = "draft"
+        case pending = "pending"
+        case priv = "private"
+    }
+    enum AllowedStatus: String, Codable {
+        case open
+        case closed
+    }
+    enum Format: String, Codable {
+        case standard
+        case aside
+        case chat
+        case gallery
+        case link
+        case image
+        case quote
+        case status
+        case video
+        case audio
+    }
+    struct PostText: Codable {
+        let rendered: String
+        let protected: Bool
+    }
+    struct Rendered: Codable {
+        let rendered: String
+    }
+    /// The date the object was published, in the site's timezone.
+    let date: Date
+    /// The globally unique identifier for the object.
+    let guid: Rendered
+    /// Unique identifier for the object.
+    let id: Int
+    /// URL to the object.
+    let link: URL
+    /// The date the object was last modified, in the site's timezone.
+    let modified: Date
+    /// An alphanumeric identifier for the object unique to its type.
+    let slug: String
+    /// A named status for the object.
+    let status: Status
+    /// Type of Post for the object.
+    let type: String
+    /// The title for the object.
+    let title: Rendered
+    /// The content for the object.
+    let content: PostText
+    /// The ID for the author of the object.
+    let author: Int
+    /// The excerpt for the object.
+    let excerpt: PostText
+    /// The ID of the featured media for the object.
+    let featuredMedia: Int
+    /// Whether or not comments are open on the object.
+    let commentStatus: AllowedStatus
+    /// Whether or not the object can be pinged.
+    let pingStatus: AllowedStatus
+    /// The order of the object in relation to other object of its type.
+    let menuOrder: Int
+    /// The theme file to use to display the object.
+    let template: String
     
+    enum CodingKeys: String, CodingKey {
+        case id, link, slug, status, type, title, content, author, excerpt, template
+        case date = "date_gmt"
+        case guid = "guid"
+        case modified = "modified_gmt"
+        case featuredMedia = "featured_media"
+        case commentStatus = "comment_status"
+        case pingStatus = "ping_status"
+        case menuOrder = "menu_order"
+    }
 }
