4bb25369d54057c0c6402e2c604fd2039c311538
jonathanmonreal <jem177@case.edu>
2018-08-28 22:25:50 -0600

    add BytePushCategory

---
+++ BytePush/BytePushCategory.swift
@@ -0,0 +1,13 @@
+//
+//  BytePushCategory.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 8/28/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
+struct BytePushCategory: Codable {
+    
+}
