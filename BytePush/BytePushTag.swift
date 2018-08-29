1ff69a65dd25188661145493e5683dc477cb4ac2
jonathanmonreal <jem177@case.edu>
2018-08-28 22:48:55 -0600

    add BytePushTag

---
+++ BytePush/BytePushTag.swift
@@ -0,0 +1,13 @@
+//
+//  BytePushTag.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 8/28/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
+public struct BytePushTag: Codable {
+    
+}
