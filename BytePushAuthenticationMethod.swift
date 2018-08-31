f8fd3a2fa7ebe86404882d085944b0846edf26fa
jemdevelop <contact@jemdevelop.com>
2018-08-31 10:48:46 -0600

    add BytePushAuthenticationMethod protocol

---
+++ BytePushAuthenticationMethod.swift
@@ -0,0 +1,13 @@
+//
+//  BytePushAuthenticationMethod.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 8/31/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
+protocol BytePushAuthenticationMethod {
+    
+}
