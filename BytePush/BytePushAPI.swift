d12c07a18fbdc90ec7fc329f7366bd5fade94ea9
jonathanmonreal <jem177@case.edu>
2018-08-30 19:37:00 -0600

    add Endpoint enum, authenticator, computed properties to BytePushAPI

--- BytePush/BytePushAPI.swift
+++ BytePush/BytePushAPI.swift
@@ -8,8 +8,28 @@
 
 import Foundation
 
+enum Endpoint: String {
+    case posts = "/wp/v2/posts"
+    case categories = "/wp/v2/categories"
+    case tags = "/wp/v2/tags"
+    case pages = "/wp/v2/pages"
+    case comments = "/wp/v2/comments"
+    case media = "/wp/v2/media"
+    case users = "/wp/v2/users"
+}
+
 public class BytePushAPI {
+    /// The host address of the blog.
     let host: URL
+    var authenticator: BytePushAuthenticator
+    /// The login page URL of the blog.
+    var loginPage: URL {
+        return URL(string: "/wp-login.php", relativeTo: host)!
+    }
+    /// The admin page URL of the blog.
+    var adminPage: URL {
+        return URL(string: "wp-admin/admin.php", relativeTo: host)!
+    }
     
     init(blogURL host: URL) {
         self.host = host
