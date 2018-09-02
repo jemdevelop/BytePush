0880faebd83b3fcd91f50e6099255f20f0a64ac0
jemdevelop <contact@jemdevelop.com>
2018-09-01 19:27:45 -0600

    add comments

--- BytePush/QueryTypes/BytePushCommentQuery.swift
+++ BytePush/QueryTypes/BytePushCommentQuery.swift
@@ -13,25 +13,45 @@ struct BytePushCommentQuery: WordPressQuery {
         case date, id, include, post, parent, type
         case dateGMT = "date_gmt"
     }
+    /// Scope under which the request is made; determines fields present in response.
     var context: Context?
+    /// Current page of the collection.
     var page: Int?
+    /// Maximum number of items to be returned in result set.
     var perPage: Int?
+    /// Limit results to those matching a string.
     var search: String?
+    /// Limit response to comments published after a given date.
     var after: Date?
+    /// Limit result set to comments assigned to specific user IDs. Requires authorization.
     var author: [Int]?
+    /// Ensure result set excludes comments assigned to specific user IDs. Requires authorization.
     var authorExclude: [Int]?
+    /// Limit result set to that from a specific author email. Requires authorization.
     var authorEmail: [String]?
+    /// Limit response to comments published before a given ISO8601 compliant date.
     var before: Date?
+    /// Ensure result set excludes specific IDs.
     var exclude: [Int]?
+    /// Limit result set to specific IDs.
     var include: [Int]?
+    /// Offset the result set by a specific number of items.
     var offset: Int?
+    /// Order sort attribute ascending or descending.
     var order: Order?
+    /// Sort collection by object attribute.
     var orderby: Orderby?
+    /// Limit result set to comments of specific parent IDs.
     var parent: [Int]?
+    /// Ensure result set excludes specific parent IDs.
     var parentExclude: [Int]?
+    /// Limit result set to comments assigned to specific post IDs.
     var post: [Int]?
+    /// Limit result set to comments assigned a specific status. Requires authorization.
     var status: String?
+    /// Limit result set to comments assigned a specific type. Requires authorization.
     var type: String?
+    /// The password for the post if it is password protected.
     var password: String?
     
     enum CodingKeys: String, CodingKey {
