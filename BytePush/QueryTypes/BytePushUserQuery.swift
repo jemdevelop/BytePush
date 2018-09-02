6dec8893d3ae48a599bc25d3be20c14729263a15
jemdevelop <contact@jemdevelop.com>
2018-09-01 21:29:58 -0600

    add BytePushUserQuery

---
+++ BytePush/QueryTypes/BytePushUserQuery.swift
@@ -0,0 +1,27 @@
+//
+//  BytePushUserQuery.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 9/1/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
+struct BytePushUserQuery: WordPressQuery {
+    enum Orderby: String, Codable {
+        case id, include, name, slug, email, url
+        case registeredDate = "registered_date"
+    }
+    var context: Context?
+    var page: Int?
+    var perPage: Int?
+    var search: String?
+    var exclude: [Int]?
+    var include: [Int]?
+    var offset: Int?
+    var order: Order?
+    var orderby: Orderby?
+    var slug: [String]?
+    var roles: String?
+}
