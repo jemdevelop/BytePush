6fca9f92f04d598ebe3a5dbe43a926f3cec1fedc
jemdevelop <contact@jemdevelop.com>
2018-09-01 19:36:05 -0600

    add properties

--- BytePush/QueryTypes/BytePushMediaQuery.swift
+++ BytePush/QueryTypes/BytePushMediaQuery.swift
@@ -9,5 +9,29 @@
 import Foundation
 
 struct BytePushMediaQuery: WordPressQuery {
-    
+    enum Orderby: String, Codable {
+        case author, date, id, include, modified, parent, relevance, slug, title
+    }
+    enum MediaType: String, Codable {
+        case image, video, audio, application
+    }
+    var context: Context?
+    var page: Int?
+    var perPage: Int?
+    var search: String?
+    var after: Date?
+    var author: [Int]?
+    var authorExclude: [Int]?
+    var before: Date?
+    var exclude: [Int]?
+    var include: [Int]?
+    var offset: Int?
+    var order: Order?
+    var orderby: Orderby?
+    var parent: [Int]?
+    var parentExclude: [Int]?
+    var slug: [String]?
+    var status: [String]?
+    var mediaType: MediaType?
+    var mimeType: String?
 }
