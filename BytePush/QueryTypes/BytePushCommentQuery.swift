b0f8eabe60dda2fca06200ea3b5dbef886e74d19
jemdevelop <contact@jemdevelop.com>
2018-09-01 19:15:53 -0600

    add BytePushCommentQuery

---
+++ BytePush/QueryTypes/BytePushCommentQuery.swift
@@ -0,0 +1,13 @@
+//
+//  BytePushCommentQuery.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 9/1/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
+struct BytePushCommentQuery: WordPressQuery {
+    
+}
