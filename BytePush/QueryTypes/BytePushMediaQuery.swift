620b103d1aa6c989952436c2ee1f77923340e782
jemdevelop <contact@jemdevelop.com>
2018-09-01 19:40:07 -0600

    add comments

--- BytePush/QueryTypes/BytePushMediaQuery.swift
+++ BytePush/QueryTypes/BytePushMediaQuery.swift
@@ -15,24 +15,43 @@ struct BytePushMediaQuery: WordPressQuery {
     enum MediaType: String, Codable {
         case image, video, audio, application
     }
+    /// Scope under which the request is made; determines fields present in response.
     var context: Context?
+    /// Current page of the collection.
     var page: Int?
+    /// Maximum number of items to be returned in result set.
     var perPage: Int?
+    /// Limit results to those matching a string.
     var search: String?
+    /// Limit response to posts published after a given date.
     var after: Date?
+    /// Limit result set to posts assigned to specific authors.
     var author: [Int]?
+    /// Ensure result set excludes posts assigned to specific authors.
     var authorExclude: [Int]?
+    /// Limit response to posts published before a given date.
     var before: Date?
+    /// Ensure result set excludes specific IDs.
     var exclude: [Int]?
+    /// Limit result set to specific IDs.
     var include: [Int]?
+    /// Offset the result set by a specific number of items.
     var offset: Int?
+    /// Order sort attribute ascending or descending.
     var order: Order?
+    /// Sort collection by object attribute.
     var orderby: Orderby?
+    /// Limit result set to items with particular parent IDs.
     var parent: [Int]?
+    /// Limit result set to all items except those of a particular parent ID.
     var parentExclude: [Int]?
+    /// Limit result set to posts with one or more specific slugs.
     var slug: [String]?
+    /// Limit result set to posts assigned one or more statuses.
     var status: [String]?
+    /// Limit result set to attachments of a particular media type.
     var mediaType: MediaType?
+    /// Limit result set to attachments of a particular MIME type.
     var mimeType: String?
     
     enum CodingKeys: String, CodingKey {
