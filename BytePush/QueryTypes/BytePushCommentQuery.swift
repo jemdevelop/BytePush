7089e2455ebdfb33f4fe8249adec68d256f83718
jemdevelop <contact@jemdevelop.com>
2018-09-01 19:21:39 -0600

    add properties

--- BytePush/QueryTypes/BytePushCommentQuery.swift
+++ BytePush/QueryTypes/BytePushCommentQuery.swift
@@ -9,5 +9,28 @@
 import Foundation
 
 struct BytePushCommentQuery: WordPressQuery {
-    
+    enum Orderby: String, Codable {
+        case date, id, include, post, parent, type
+        case dateGMT = "date_gmt"
+    }
+    var context: Context?
+    var page: Int?
+    var perPage: Int?
+    var search: String?
+    var after: Date?
+    var author: [Int]?
+    var authorExclude: [Int]?
+    var authorEmail: [String]?
+    var before: Date?
+    var exclude: [Int]?
+    var include: [Int]?
+    var offset: Int?
+    var order: Order?
+    var orderby: Orderby?
+    var parent: [Int]?
+    var parentExclude: [Int]?
+    var post: [Int]?
+    var status: String?
+    var type: String?
+    var password: String?
 }
