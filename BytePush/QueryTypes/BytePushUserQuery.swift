cf145ed3d28815cd0ff8238172cd7c16a815aa9d
jemdevelop <contact@jemdevelop.com>
2018-09-01 21:35:06 -0600

    add comments

--- BytePush/QueryTypes/BytePushUserQuery.swift
+++ BytePush/QueryTypes/BytePushUserQuery.swift
@@ -13,17 +13,28 @@ struct BytePushUserQuery: WordPressQuery {
         case id, include, name, slug, email, url
         case registeredDate = "registered_date"
     }
+    /// Scope under which the request is made; determines fields present in response.
     var context: Context?
+    /// Current page of the collection.
     var page: Int?
+    /// Maximum number of items to be returned in result set.
     var perPage: Int?
+    /// Limit results to those matching a string.
     var search: String?
+    /// Ensure result set excludes specific IDs.
     var exclude: [Int]?
+    /// Limit result set to specific IDs.
     var include: [Int]?
+    /// Offset the result set by a specific number of items.
     var offset: Int?
+    /// Order sort attribute ascending or descending.
     var order: Order?
+    /// Sort collection by object attribute.
     var orderby: Orderby?
+    /// Limit result set to users with one or more specific slugs.
     var slug: [String]?
-    var roles: String?
+    /// Limit result set to users matching at least one specific role provided.
+    var roles: [String]?
     
     enum CodingKeys: String, CodingKey {
         case context, page, search, exclude, include, offset, order, orderby
