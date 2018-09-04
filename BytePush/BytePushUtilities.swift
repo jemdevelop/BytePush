75001c0177a0aa313b834fd2ac8cf2cad9d87c49
jemdevelop <contact@jemdevelop.com>
2018-09-04 00:36:48 -0600

    fix formatter var

--- BytePush/BytePushUtilities.swift
+++ BytePush/BytePushUtilities.swift
@@ -11,8 +11,8 @@ import Foundation
 struct BytePushUtilities {
     static var wpDateFormatter: DateFormatter = {
         var formatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         return formatter
     }()
 }
