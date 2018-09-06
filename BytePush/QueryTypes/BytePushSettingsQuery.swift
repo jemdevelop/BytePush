be36f3595a7da29f5b4dea6949b26ba528cd6fc6
jemdevelop <contact@jemdevelop.com>
2018-09-05 19:23:23 -0600

    implement BytePushSettingsQuery

--- BytePush/QueryTypes/BytePushSettingsQuery.swift
+++ BytePush/QueryTypes/BytePushSettingsQuery.swift
@@ -8,6 +8,38 @@
 
 import Foundation
 
-public struct BytePushSettingsQuery {
+public struct BytePushSettingsQuery: WordPressQuery {
+    public var wpEndpointURL: URL
     
+    public init(withEndpointURL wpEndpointURL: URL) {
+        self.wpEndpointURL = wpEndpointURL
+    }
+    
+    public func execute(withAuthenticationItem authenticationItem: URLQueryItem? = nil, result: @escaping (WordPressQueryResult<BytePushPostStatus>) -> Void) {
+        guard var components = URLComponents(url: wpEndpointURL, resolvingAgainstBaseURL: false) else {
+            result(.failure(WordPressQueryError.couldNotConstructURL))
+            return
+        }
+        if let item = authenticationItem {
+            components.queryItems = [item]
+        }
+        guard let url = components.url else {
+            result(.failure(WordPressQueryError.couldNotConstructURL))
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                do {
+                    let decoder = BytePushUtilities.wpJSONDecoder
+                    let requestResult = try decoder.decode([BytePushPostStatus].self, from: data)
+                    result(.success(requestResult))
+                } catch let err {
+                    result(.failure(err))
+                }
+            }
+            }.resume()
+    }
 }
