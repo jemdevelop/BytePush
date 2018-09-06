ee51bef9b7f38ea89787ffc2e37609ed9376a0a4
jemdevelop <contact@jemdevelop.com>
2018-09-05 18:42:48 -0600

    add comments

--- BytePush/DataTypes/BytePushPostStatus.swift
+++ BytePush/DataTypes/BytePushPostStatus.swift
@@ -9,12 +9,19 @@
 import Foundation
 
 struct BytePushPostStatus: Codable {
+    /// The title for the status.
     public let name: String?
+    /// Whether posts with this status should be private.
     public let privateStatus: Bool?
+    /// Whether posts with this status should be protected.
     public let protectedStatus: Bool?
+    /// Whether posts of this status should be shown in the front end of the site.
     public let publicStatus: Bool?
+    /// Whether posts with this status should be publicly-queryable.
     public let queryable: Bool?
+    /// Whether to include posts in the edit listing for their post type.
     public let showInList: Bool?
+    /// An alphanumeric identifier for the status.
     public let slug: String?
     
     private enum CodingKeys: String, CodingKey {
