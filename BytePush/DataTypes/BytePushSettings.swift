d01b3a0fea9fc8d4b74aea1a5b9a4e27e1dd67ee
jemdevelop <contact@jemdevelop.com>
2018-09-05 19:13:52 -0600

    add comments

--- BytePush/DataTypes/BytePushSettings.swift
+++ BytePush/DataTypes/BytePushSettings.swift
@@ -9,18 +9,31 @@
 import Foundation
 
 public struct BytePushSettings: Codable {
+    /// Site title.
     public let title: String?
+    /// Site tagline.
     public let description: String?
+    /// A city in the same timezone as you.
     public let timezone: TimeZone?
+    /// A date format for all date strings.
     public let dateFormat: String?
+    /// A time format for all time strings.
     public let timeFormat: String?
+    /// A day number of the week that the week should start on.
     public let startOfWeek: Int?
+    /// WordPress locale code.
     public let language: Locale?
+    /// Convert emoticons like :-) and :-P to graphics on display.
     public let useSmilies: Bool?
+    /// Default post category.
     public let defaultCategory: Int?
+    /// Default post format.
     public let defaultPostFormat: String?
+    /// Blog pages show at most.
     public let postsPerPage: Int?
+    /// Allow link notifications from other blogs (pingbacks and trackbacks) on new articles.
     public let defaultPingStatus: WPAllowedStatus?
+    /// Allow people to post comments on new articles.
     public let defaultCommentStatus: WPAllowedStatus?
     
     private enum CodingKeys: String, CodingKey {
