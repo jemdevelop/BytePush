f1a799ebf81aa076f2621469fb00bf773e3c82ff
jemdevelop <contact@jemdevelop.com>
2018-09-05 19:12:07 -0600

    implement BytePushSettings

--- BytePush/DataTypes/BytePushSettings.swift
+++ BytePush/DataTypes/BytePushSettings.swift
@@ -8,6 +8,31 @@
 
 import Foundation
 
-public struct BytePushSettings {
+public struct BytePushSettings: Codable {
+    public let title: String?
+    public let description: String?
+    public let timezone: TimeZone?
+    public let dateFormat: String?
+    public let timeFormat: String?
+    public let startOfWeek: Int?
+    public let language: Locale?
+    public let useSmilies: Bool?
+    public let defaultCategory: Int?
+    public let defaultPostFormat: String?
+    public let postsPerPage: Int?
+    public let defaultPingStatus: WPAllowedStatus?
+    public let defaultCommentStatus: WPAllowedStatus?
     
+    private enum CodingKeys: String, CodingKey {
+        case title, description, timezone, language
+        case dateFormat = "date_format"
+        case timeFormat = "time_format"
+        case startOfWeek = "start_of_week"
+        case useSmilies = "use_smilies"
+        case defaultCategory = "default_category"
+        case defaultPostFormat = "default_post_format"
+        case postsPerPage = "posts_per_page"
+        case defaultPingStatus = "default_ping_status"
+        case defaultCommentStatus = "default_comment_status"
+    }
 }
