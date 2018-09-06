bc430895ced8767732c870d6103a3af03ac56edb
jemdevelop <contact@jemdevelop.com>
2018-09-05 18:57:00 -0600

    add BytePushSettings

---
+++ BytePush/DataTypes/BytePushSettings.swift
@@ -0,0 +1,13 @@
+//
+//  BytePushSettings.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 9/5/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
+public struct BytePushSettings {
+    
+}
