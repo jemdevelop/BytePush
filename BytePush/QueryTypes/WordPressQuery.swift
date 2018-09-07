0e923a09931c5042c5b8546e247611d76160406d
jemdevelop <contact@jemdevelop.com>
2018-09-07 14:39:42 -0600

    move queryItems to a WordPressOperation extension

--- BytePush/QueryTypes/WordPressQuery.swift
+++ BytePush/QueryTypes/WordPressQuery.swift
@@ -33,29 +33,6 @@ public protocol WordPressQuery: WordPressOperation {
 }
 
 extension WordPressQuery {
-    /// An array of query items representing the query.
-    var queryItems: [URLQueryItem]? {
-        let encoder = JSONEncoder()
-        let dateFormatter = BytePushUtilities.wpDateFormatter
-        encoder.dateEncodingStrategy = .formatted(dateFormatter)
-        guard
-            let json = try? encoder.encode(self),
-            let serialized = try? JSONSerialization.jsonObject(with: json, options: []),
-            let dict = serialized as? [String:Any]
-            else {
-                return nil
-        }
-        return dict.map {
-            if let array = $0.value as? [Any] {
-                // Arrays must be converted to comma-separated value parameters
-                let newValue = array.map { "\($0)" }.joined(separator: ",")
-                return URLQueryItem(name: $0.key, value: newValue)
-            } else {
-                return URLQueryItem(name: $0.key, value: "\($0.value)")
-            }
-        }
-    }
-    
     /// The complete URL for the query.
     public var queryURL: URL? {
         var components = URLComponents(url: wpEndpointURL, resolvingAgainstBaseURL: false)
