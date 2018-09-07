5a4e584e018e8cf2cedf5554fbfdff3108fd5581
jemdevelop <contact@jemdevelop.com>
2018-09-07 15:07:32 -0600

    add BytePushCategoryRetrieve

---
+++ BytePush/RetrieveTypes/BytePushCategoryRetrieve.swift
@@ -0,0 +1,27 @@
+//
+//  BytePushCategoryRetrieve.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 9/7/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
+public struct BytePushCategoryRetrieve: WordPressRetrieve {
+    /// The blog URL with complete path to the appropriate endpoint.
+    public let wpEndpointURL: URL
+    /// Unique identifier for the object.
+    public let id: Int
+    /// Scope under which the request is made; determines fields present in response.
+    public var context: Context?
+    
+    public init(withEndpointURL wpEndpointURL: URL, getID id: Int) {
+        self.wpEndpointURL = wpEndpointURL
+        self.id = id
+    }
+    
+    public func execute(withAuthenticationItem item: URLQueryItem?, result: @escaping (WordPressRetrieveResult<BytePushCategory>) -> Void) {
+        
+    }
+}
