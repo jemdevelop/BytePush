1a24315bd22b124deb90d7fa2c20ea92a406ccd7
jemdevelop <contact@jemdevelop.com>
2018-09-07 15:44:21 -0600

    add BytePushSettingsRetrieve

---
+++ BytePush/RetrieveTypes/BytePushSettingsRetrieve.swift
@@ -0,0 +1,25 @@
+//
+//  BytePushSettingsRetrieve.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 9/7/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
+public struct BytePushSettingsRetrieve: WordPressRetrieve {
+    /// The blog URL with complete path to the appropriate endpoint.
+    public let wpEndpointURL: URL
+    public var idString: String {
+        return ""
+    }
+    
+    public init(withEndpointURL wpEndpointURL: URL) {
+        self.wpEndpointURL = wpEndpointURL
+    }
+    
+    public func execute(withAuthenticationItem item: URLQueryItem?, result: @escaping (WordPressRetrieveResult<BytePushSettings>) -> Void) {
+        
+    }
+}
