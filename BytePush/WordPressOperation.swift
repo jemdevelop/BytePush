46c5136cd568730e351f9af8e3327e4bed6332c0
jemdevelop <contact@jemdevelop.com>
2018-09-07 08:22:57 -0600

    add WordPressOperation

---
+++ BytePush/WordPressOperation.swift
@@ -0,0 +1,14 @@
+//
+//  WordPressOperation.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 9/7/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
+public protocol WordPressOperation: Encodable {
+    var wpEndpointURL: URL { get set }
+    init(withEndpointURL wpEndpointURL: URL)
+}
