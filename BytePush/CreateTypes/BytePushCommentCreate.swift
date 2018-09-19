0d1f244ef729d807b1cf55f2443cc8a6e36391df
jemdevelop <contact@jemdevelop.com>
2018-09-19 11:21:16 -0600

    add BytePushCommentCreate

---
+++ BytePush/CreateTypes/BytePushCommentCreate.swift
@@ -0,0 +1,10 @@
+//
+//  BytePushCommentCreate.swift
+//  BytePush
+//
+//  Created by Jonathan Monreal on 9/19/18.
+//  Copyright © 2018 Jonathan Monreal. All rights reserved.
+//
+
+import Foundation
+
