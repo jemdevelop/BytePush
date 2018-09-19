784bb86d7e59acbacab11b1b162e9d930972bacb
jemdevelop <contact@jemdevelop.com>
2018-09-19 13:02:38 -0600

    add documentation

--- BytePush/CreateTypes/BytePushCommentCreate.swift
+++ BytePush/CreateTypes/BytePushCommentCreate.swift
@@ -10,17 +10,29 @@ import Foundation
 
 public class BytePushCommentCreate {
     public let wpEndpointURL: URL
+    /// The ID of the user object, if author was a user.
     public var author: Int?
+    /// Email address for the object author.
     public var authorEmail: String
+    /// IP address for the object author.
     public var authorIP: String?
+    /// Display name for the object author.
     public var authorName: String
+    /// URL for the object author.
     public var authorURL: URL?
+    /// User agent for the object author.
     public var authorUserAgent: String?
+    /// The content for the object.
     public var content: WPContent
+    /// The date the object was published, in the site's timezone.
     public var date: Date?
+    /// The date the object was published, as GMT.
     public var dateGMT: Date?
+    /// The ID for the parent of the object.
     public var parent: Int?
+    /// The ID of the associated post object.
     public var post: Int
+    /// State of the object.
     public var status: String?
     
     public init(withEndpointURL wpEndpointURL: URL, authorName: String, authorEmail: String, content: WPContent, post: Int) {
